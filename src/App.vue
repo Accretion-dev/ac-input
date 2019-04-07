f287fe63013baacf792a70e3396646d3632087c5
Fmajor <wujinnnnn@gmail.com>
2019-04-07 17:22:23 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1> Test enviroment for ac-input</h1>
+    <ac-input/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
